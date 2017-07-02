6438ca20d8e0eb7b791ef489e9b168ea4d0f008e
Charles Bayley <cbayley@linnie.mintie.commie.com>
2017-07-02 14:04:25 -0400

    Updated my prime generator to only check numbers of the form 6n+-1 instead of all numbers

--- library/primetools.py
+++ library/primetools.py
@@ -9,7 +9,7 @@ from itertools import count
 def is_prime(n):
     if n < 2:
         return False
-    elif n == 2:
+    elif n in [2, 3]:
         return True
     for i in range(2, int(n / 2) + 1):
         if n % i == 0:
@@ -17,7 +17,13 @@ def is_prime(n):
     return True
 
 def primes():
-    for i in count(0):
+    yield 2
+    yield 3
+    def prime_guess():
+        for i in count(1):
+            yield 6*i - 1
+            yield 6*i + 1
+    for i in prime_guess():
         if is_prime(i):
             yield i
 
